b11713644943904ac360382bae53ac61c5d2581a
olaniyanpelumi <olaniyanp11@gmail.com>
2024-01-21 01:04:12 +0100

    routes correction

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 router.get("/login", (req, res) => {
-  res.render("Login");
+  res.render("login");
 });
 router.get("/signup", (req, res) => {
   res.render("signup");
